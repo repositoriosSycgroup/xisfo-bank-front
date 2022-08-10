deac0568595e2c39148ed0204fd8246e357df2fe
desarrolloFront1 <desarrollo.front@sycgroup.co>
2022-08-10 11:53:05 -0500

    se generan cambios

--- src/app/sections/teams/teams.component.ts
+++ src/app/sections/teams/teams.component.ts
@@ -3,40 +3,86 @@ import {
   ElementRef,
   ViewChildren,
   QueryList,
-  AfterViewInit, OnInit, OnDestroy
-} from "@angular/core";
+  AfterViewInit,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import {
   style,
   animate,
   AnimationBuilder,
-  AnimationPlayer
-} from "@angular/animations";
-import {Subscription} from "rxjs";
-
+  AnimationPlayer,
+} from '@angular/animations';
+import { Subscription } from 'rxjs';
+import { SwiperComponent } from 'swiper/angular';
+import SwiperCore, { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.sass']
+  styleUrls: ['./teams.component.sass'],
 })
 export class TeamsComponent implements OnInit, AfterViewInit, OnDestroy {
   subscription$!: Subscription;
   element: any;
   @ViewChildren('element') itemsView!: QueryList<ElementRef>;
+  @ViewChild('swiper', { static: false }) swiper!: SwiperComponent;
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
 
   teams: any = [
-    {"id": 0, "img": "../../../assets/img/Team/james_moreno.webp", "name": "Desarrollador de Negocios Internacionales"},
-    {"id": 2, "img": "../../../assets/img/Team/carlos_quintero.webp", "name": "CEO"},
-    {"id": 4, "img": "../../../assets/img/Team/stefania_castano.webp", "name": "Gerente Administrativa"},
-    {"id": 6, "img": "../../../assets/img/Team/don_carlos.webp", "name": "Gerente Financiero"},
-    {"id": 8, "img": "../../../assets/img/Team/vanesa_parra.webp", "name": "Comunicaciones y Relaciones Públicas"},
-    {"id": 10, "img": "../../../assets/img/Team/danitza_naranjo.webp", "name": "Líder Comercial"},
-    {"id": 12, "img": "../../../assets/img/Team/yesica_guzman.webp", "name": "Directora de Mercadeo"},
-    {"id": 14, "img": "../../../assets/img/Team/jhon_santos.webp", "name": "Líder de T.I"},
-    {"id": 16, "img": "../../../assets/img/Team/aleja_marin.webp", "name": "Directora de Recursos Humanos"},
-
-  ]
-
+    {
+      id: 0,
+      img: '../../../assets/img/Team/james_moreno.webp',
+      name: 'Desarrollador de Negocios Internacionales',
+    },
+    {
+      id: 2,
+      img: '../../../assets/img/Team/carlos_quintero.webp',
+      name: 'CEO',
+    },
+    {
+      id: 4,
+      img: '../../../assets/img/Team/stefania_castano.webp',
+      name: 'Gerente Administrativa',
+    },
+    {
+      id: 6,
+      img: '../../../assets/img/Team/don_carlos.webp',
+      name: 'Gerente Financiero',
+    },
+    {
+      id: 8,
+      img: '../../../assets/img/Team/vanesa_parra.webp',
+      name: 'Comunicaciones y Relaciones Públicas',
+    },
+    {
+      id: 10,
+      img: '../../../assets/img/Team/danitza_naranjo.webp',
+      name: 'Líder Comercial',
+    },
+    {
+      id: 12,
+      img: '../../../assets/img/Team/yesica_guzman.webp',
+      name: 'Directora de Mercadeo',
+    },
+    {
+      id: 14,
+      img: '../../../assets/img/Team/jhon_santos.webp',
+      name: 'Líder de T.I',
+    },
+    {
+      id: 16,
+      img: '../../../assets/img/Team/aleja_marin.webp',
+      name: 'Directora de Recursos Humanos',
+    },
+  ];
 
   private player!: AnimationPlayer;
 
@@ -51,26 +97,24 @@ export class TeamsComponent implements OnInit, AfterViewInit, OnDestroy {
   selectedIndex = 0;
 
   movements = [
-    {pos: 0, right: [1, 2], left: [17, 16]},
-    {pos: 2, right: [3, 4], left: [1, 0]},
-    {pos: 4, right: [5, 6], left: [3, 2]},
-    {pos: 6, right: [7, 8], left: [5, 4]},
-    {pos: 8, right: [9, 10], left: [7, 6]},
-    {pos: 10, right: [11, 12], left: [9, 8]},
-    {pos: 12, right: [13, 14], left: [11, 10]},
-    {pos: 14, right: [15, 16], left: [13, 12]},
-    {pos: 16, right: [17, 0], left: [15, 14]},
+    { pos: 0, right: [1, 2], left: [17, 16] },
+    { pos: 2, right: [3, 4], left: [1, 0] },
+    { pos: 4, right: [5, 6], left: [3, 2] },
+    { pos: 6, right: [7, 8], left: [5, 4] },
+    { pos: 8, right: [9, 10], left: [7, 6] },
+    { pos: 10, right: [11, 12], left: [9, 8] },
+    { pos: 12, right: [13, 14], left: [11, 10] },
+    { pos: 14, right: [15, 16], left: [13, 12] },
+    { pos: 16, right: [17, 0], left: [15, 14] },
   ];
 
   constructor(private builder: AnimationBuilder) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   animateViews(direction: string) {
-    let element = document.querySelectorAll<Element>(".author_carousel");
-     element.forEach((item: any) => item.style.display = "none")
+    let element = document.querySelectorAll<Element>('.author_carousel');
+    element.forEach((item: any) => (item.style.display = 'none'));
 
     this.animates.forEach((x: number, index: number) => {
       const mov: any = this.movements.find((m) => m.pos == x);
@@ -83,15 +127,15 @@ export class TeamsComponent implements OnInit, AfterViewInit, OnDestroy {
         const applystyle = {
           left: -this.cellWidth / 2 + this.radius * Math.sin(angle) + 'px',
           transform: 'scale(' + scale + ')',
-          filter: angle == 0? " " : "saturate(70%)",
+          filter: angle == 0 ? ' ' : 'saturate(70%)',
           position: 'absolute',
           'z-index': Math.floor(100 * scale),
         };
 
-        if(angle == 0){
-          const result : any = this.movements[index];
+        if (angle == 0) {
+          const result: any = this.movements[index];
           this.element = document.getElementById(result.pos) as HTMLElement;
-          this.element.style.display = "block";
+          this.element.style.display = 'block';
         }
 
         return animate(
@@ -119,13 +163,19 @@ export class TeamsComponent implements OnInit, AfterViewInit, OnDestroy {
     this.animateViews('left');
   }
 
-
   ngAfterViewInit() {
     this.cellWidth = this.itemsView.first.nativeElement.offsetWidth;
     this.animateViews('left');
   }
 
   ngOnDestroy() {
-    this.subscription$.unsubscribe()
+    this.subscription$.unsubscribe();
+  }
+
+  slideNext() {
+    this.swiper.swiperRef.slideNext(100);
+  }
+  slidePrev() {
+    this.swiper.swiperRef.slidePrev(100);
   }
 }
