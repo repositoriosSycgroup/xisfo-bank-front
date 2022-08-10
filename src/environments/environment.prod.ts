dc36c64ef30564b4dc5cb3a108dcab4665984a79
Jhoan <soporte.desarrollo@sycgroup.co>
2022-08-10 14:02:11 -0500

    ajustes de la ruta del back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
 
-  regrexEmail: '[a-zñA-ZÑ 0-9._%+-]{3,100}@[a-zñA-ZÑ0-9.-]{3,30}\.[a-zA-Z]{2,6}$',
+  regrexEmail:
+    '[a-zñA-ZÑ 0-9._%+-]{3,100}@[a-zñA-ZÑ0-9.-]{3,30}.[a-zA-Z]{2,6}$',
   regrexName: '[A-ZÑ,a-zñ, ,á,é,í,ó,ú]{3,}$',
-  sendEmailUrl: 'http://127.0.0.1:3000/envio'
+  sendEmailUrl: 'https://xisfobankcorreo.xisfo.co/envio',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,10 @@ export const environment = {
   production: true,
 
   //variables de validacion
-  regrexEmail: '[a-zñA-ZÑ 0-9._%+-]{3,100}@[a-zñA-ZÑ0-9.-]{3,30}\.[a-zA-Z]{2,6}$',
+  regrexEmail:
+    '[a-zñA-ZÑ 0-9._%+-]{3,100}@[a-zñA-ZÑ0-9.-]{3,30}.[a-zA-Z]{2,6}$',
   regrexName: '[A-ZÑ,a-zñ, ,á,é,í,ó,ú]{3,}$',
-  sendEmailUrl: 'http://127.0.0.1:3000/envio'
+  sendEmailUrl: 'https://xisfobankcorreo.xisfo.co/envio',
 };
 
 /*
